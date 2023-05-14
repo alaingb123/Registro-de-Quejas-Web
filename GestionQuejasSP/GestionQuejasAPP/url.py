0e58a33444cd06c8e7fdcce41472c4c3700f0953
Alain <alaingalvez76@gmail.com>
2023-05-14 12:42:39 -0400

    Formulario Insertar Queja

--- GestionQuejasSP/GestionQuejasAPP/url.py
+++ GestionQuejasSP/GestionQuejasAPP/url.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import dash
+from .views import dash,InsertarQueja
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('dash/',login_required(dash),name="dash"),
+    path('',login_required(dash),name="dash"),
+    path('insertarQueja/',InsertarQueja,name="insertar"),
 ]
